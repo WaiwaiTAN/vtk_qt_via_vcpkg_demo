0cea0933563565bcc4a8f64fcfa0fe92d48301fd
Zheng TAN <a.j.fikrytan@sjtu.edu.cn>
2021-03-14 11:41:02 +0800

    vtk[qt] development environment configured.

--- vcpkg64qt/Qt64vcpkgr.h
+++ vcpkg64qt/Qt64vcpkgr.h
@@ -1,15 +1,17 @@
 #pragma once
-
+#include <vtk-9.0/vtkSmartPointer.h>
 #include <QtWidgets/QMainWindow>
 #include "ui_Qt64vcpkgr.h"
 
-class Qt64vcpkgr : public QMainWindow
+class Qt64vcpkgr : public QMainWindow, private Ui::Qt64vcpkgrClass
 {
     Q_OBJECT
 
 public:
     Qt64vcpkgr(QWidget *parent = Q_NULLPTR);
+	~Qt64vcpkgr() = default;
 
-private:
-    Ui::Qt64vcpkgrClass ui;
+public slots:
+
+	void slotExit();
 };
